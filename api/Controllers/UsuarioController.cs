f27ea0c0909eeb6bfc41589ee86d2d0cdfbd272c
Thiago Baso <thiagorosabaso@gmail.com>
2024-01-08 22:20:05 -0300

    api add

---
+++ api/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using api.Data;
+using api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly DBContext _dbContext;
+        public UsuarioController(DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public async Task<UsuarioModel> AddUser(UsuarioModel user)
+        {
+            await _dbContext.Usuario.AddAsync(user);
+            await _dbContext.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
---
+++ api/Data/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class DBContext:DbContext 
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<UsuarioModel> Usuario { get; set; }
+    }
+}
